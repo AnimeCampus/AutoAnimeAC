1a79887c3738e682f4387c297d88d391e9f51a63
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-24 00:51:16 +0530

    Update parser.py

--- AutoAnimeBot/modules/parser.py
+++ AutoAnimeBot/modules/parser.py
@@ -3,52 +3,59 @@ from techzapi.api import TechZApi
 from AutoAnimeBot.core.log import LOGGER
 from AutoAnimeBot.modules.schedule import update_schedule
 from AutoAnimeBot.modules.db import get_animesdb, get_uploads, is_failed, save_animedb
-
+import traceback
 
 logger = LOGGER("Parser")
-
 
 async def auto_parser(TECHZ_API_KEY, app):
     Gogo = TechZApi.Gogo(TECHZ_API_KEY)
     Gogo.base = "http://api.techzbots.live"
 
     while True:
-        await app.update_status("Scrapping Animes...")
+        try:
+            await app.update_status("Scrapping Animes...")
 
-        data = Gogo.latest()
-        saved = await get_animesdb()
-        uploaded = await get_uploads()
+            data = Gogo.latest()
+            saved = await get_animesdb()
+            uploaded = await get_uploads()
 
-        saved_anime = []
-        for i in saved:
-            saved_anime.append(i["id"])
+            saved_anime = []
+            for i in saved:
+                saved_anime.append(i["id"])
 
-        uanimes = {}
-        for i in uploaded:
-            if i["id"] not in uanimes:
-                uanimes[i["id"]] = set()
-            if i.get("q"):
-                uanimes[i["id"]] = set(i["q"])
+            uanimes = {}
+            for i in uploaded:
+                if i["id"] not in uanimes:
+                    uanimes[i["id"]] = set()
+                if i.get("q"):
+                    uanimes[i["id"]] = set(i["q"])
 
-        pos = len(saved_anime) + 1
-        for i in data:
-            if i["id"] not in saved_anime:
-                if i["id"] in uanimes:
-                    if uanimes[i["id"]] == {"360p", "480p", "720p", "1080p"}:
-                        continue
+            pos = len(saved_anime) + 1
+            for i in data:
+                if i["id"] not in saved_anime:
+                    if i["id"] in uanimes:
+                        if uanimes[i["id"]] == {"360p", "480p", "720p", "1080p"}:
+                            continue
 
-                id = i["id"]
-                if not (await is_failed(id)):
-                    await save_animedb(id, pos)
-                    pos += 1
+                    id = i["id"]
+                    if not (await is_failed(id)):
+                        await save_animedb(id, pos)
+                        pos += 1
 
-        saved = await get_animesdb()
-        for i in saved:
-            if i["id"] not in app.queue:
-                app.queue.append(i["id"])
-                logger.info(f'Saved To Queue --> {i["id"]}')
+            saved = await get_animesdb()
+            for i in saved:
+                if i["id"] not in app.queue:
+                    app.queue.append(i["id"])
+                    logger.info(f'Saved To Queue --> {i["id"]}')
 
-        await update_schedule(app)
-        await app.update_status("Idle...")
+            await update_schedule(app)
+            await app.update_status("Idle...")
+
+        except Exception as e:
+            logger.error(f"Error in auto_parser: {e}")
+            traceback.print_exc()
 
         await asyncio.sleep(600)
+
+# Call the auto_parser function with appropriate arguments
+# asyncio.run(auto_parser("YOUR_TECHZ_API_KEY", your_app_object))
